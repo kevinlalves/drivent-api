b80f11e31536ac01d008dacf41f835859ab04eb4
Kevin Alves <kevin.alves.dev@gmail.com>
2023-04-23 16:46:25 -0300

    fix: hub test

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -6,18 +6,6 @@ import hotelsRepository from '@/repositories/hotels-repository';
 import { prisma } from '@/config';
 
 export async function getHotelsService(userId: number) {
-  // const user = await userRepository.findWithHotelAndTicket(userId);
-  // const booking = user.Booking[0];
-  // const enrollment = user.Enrollment[0];
-
-  // if (!booking) throw notFoundError();
-
-  // const ticket = enrollment.Ticket[0];
-
-  // if (ticket.status === TicketStatus.RESERVED || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
-  //   throw paymentRequiredError();
-  // }
-
   const ticket = await prisma.ticket.findFirst({ include: { TicketType: true } });
   if (!ticket) throw notFoundError();
 
@@ -30,20 +18,11 @@ export async function getHotelsService(userId: number) {
 }
 
 export async function getHotelService({ userId, hotelId }: GetHotelRooms) {
-  const user = await userRepository.findWithHotelAndTicket(userId);
-  const booking = user.Booking[0];
   const hotel = await hotelsRepository.findByIdWithRooms(hotelId);
-  const enrollment = user.Enrollment[0];
+  const ticket = await prisma.ticket.findFirst({ include: { TicketType: true } });
+  if (!ticket) throw notFoundError();
 
-  if (!booking || !enrollment) throw notFoundError();
-
-  const ticket = enrollment.Ticket[0];
-
-  if (booking.Room.Hotel.id !== hotelId) throw notFoundError();
-
-  if (ticket.status === TicketStatus.RESERVED || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
-    throw paymentRequiredError();
-  }
+  if (!ticket.TicketType.includesHotel || ticket.status !== TicketStatus.PAID) throw paymentRequiredError();
 
   return hotel;
 }
